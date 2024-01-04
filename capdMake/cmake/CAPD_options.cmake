49f60696c770db82181496704df7d6ecbf85ac2f
Tomasz Kapela <tomasz.kapela@gmail.com>
2024-01-04 17:35:57 +0100

    cmake options introduced and splitted

---
+++ capdMake/cmake/CAPD_options.cmake
@@ -0,0 +1,26 @@
+#option(WITH_CAPD_TESTS "Activate/deactivate tests compilation" OFF)
+#option(WITH_CAPD_EXAMPLES "Activate/deactivate tests compilation" OFF)
+
+#if( ${WITH_CAPD_TESTS} )
+#  set(BUILD_TEST_EXECUTABLES ON)
+#else ()
+#  set(BUILD_TEST_EXECUTABLES OFF)
+#endif ()
+option(CAPD_BUILD_ALL "CAPD test and examples " OFF)
+message(STATUS "Build all CAPD      : ${CAPD_BUILD_ALL}")
+option(BUILD_TEST_EXECUTABLES "Activate/deactivate CAPD tests compilation" OFF)
+option(BUILD_EXAMPLES_EXECUTABLES "Activate/deactivate CAPD examples compilation" OFF)
+
+#cmake_dependent_option(BUILD_TEST_EXECUTABLES "Activate/deactivate CAPD tests compilation" OFF "NOT CAPD_BUILD_ALL" ON )
+if( ${CAPD_BUILD_ALL} )
+  message(STATUS "TAK")
+  set(BUILD_TEST_EXECUTABLES ON)
+  set(BUILD_EXAMPLES_EXECUTABLES ON)
+endif()
+
+option(ENABLE_MULTIPRECISION "Activate/decactivate multiprecision support in CAPD" ON)
+
+message(STATUS "Build CAPD tests    : ${BUILD_TEST_EXECUTABLES}")
+message(STATUS "Build CAPD examples : ${BUILD_EXAMPLES_EXECUTABLES}")
+message(STATUS "CAPD multiprecision : ${ENABLE_MULTIPRECISION}")
+#  if ( ${BUILD_EXAMPLE_EXECUTABLES} ))
