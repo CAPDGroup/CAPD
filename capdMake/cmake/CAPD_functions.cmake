dfba0e9a2dba6d5095566e76f98810b64f37d184
Aleksander Pasiut <apasiut@protonmail.com>
2024-01-30 00:13:40 +0100

    remove unnecessary linkage directories from tests/examples

--- capdMake/cmake/CAPD_functions.cmake
+++ capdMake/cmake/CAPD_functions.cmake
@@ -1,8 +1,6 @@
-
 function(build_example_executable_monofile EXAMPLE_EXECUTABLE_NAME)
 
   add_executable(${EXAMPLE_EXECUTABLE_NAME})
-  target_link_directories(${EXAMPLE_EXECUTABLE_NAME} PRIVATE ${CMAKE_INSTALL_PREFIX}/lib)
   target_link_libraries(${EXAMPLE_EXECUTABLE_NAME} capd)
   target_sources(${EXAMPLE_EXECUTABLE_NAME} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/${EXAMPLE_EXECUTABLE_NAME}.cpp)
 
@@ -11,7 +9,6 @@ endfunction()
 function(build_test_executable_monofile TEST_PROJECT_NAME)
 
   add_executable(${TEST_PROJECT_NAME})
-  target_link_directories(${TEST_PROJECT_NAME} PRIVATE ${CMAKE_INSTALL_PREFIX}/lib)
   target_link_libraries (${TEST_PROJECT_NAME} capd)
   target_link_libraries (${TEST_PROJECT_NAME} -lstdc++)
   target_link_libraries (${TEST_PROJECT_NAME} -lm)
@@ -24,7 +21,6 @@ endfunction()
 function(build_boost_test_executable_monofile TEST_PROJECT_NAME)
 
   add_executable(${TEST_PROJECT_NAME})
-  target_link_directories(${TEST_PROJECT_NAME} PRIVATE ${CMAKE_INSTALL_PREFIX}/lib)
   target_link_libraries(${TEST_PROJECT_NAME} capd)
   find_package (Boost REQUIRED COMPONENTS unit_test_framework)
   target_link_libraries (${TEST_PROJECT_NAME} Boost::unit_test_framework)
@@ -39,7 +35,6 @@ endfunction()
 function(build_test_executable TEST_PROJECT_NAME)
 
   add_executable(${TEST_PROJECT_NAME})
-  target_link_directories(${TEST_PROJECT_NAME} PRIVATE ${CMAKE_INSTALL_PREFIX}/lib)
   target_link_libraries (${TEST_PROJECT_NAME} capd)
   target_link_libraries (${TEST_PROJECT_NAME} -lstdc++)
   target_link_libraries (${TEST_PROJECT_NAME} -lm)
@@ -55,7 +50,6 @@ endfunction()
 function(build_boost_test_executable TEST_PROJECT_NAME)
 
   add_executable(${TEST_PROJECT_NAME})
-  target_link_directories(${TEST_PROJECT_NAME} PRIVATE ${CMAKE_INSTALL_PREFIX}/lib)
   target_link_libraries(${TEST_PROJECT_NAME} capd)
   find_package (Boost REQUIRED COMPONENTS unit_test_framework)
   target_link_libraries (${TEST_PROJECT_NAME} Boost::unit_test_framework)
